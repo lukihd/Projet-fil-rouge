9c39fd0f03798523f41440f3fd8b686584cdc944
lukihd <lucas.erisset@ynov.com>
2018-03-02 10:46:36 +0100

    maj

--- sites/bdd/bdd.sql
+++ sites/bdd/bdd.sql
@@ -21,7 +21,7 @@ CREATE TABLE IF NOT EXISTS  `livre` (
   `page` INT (6) NOT NULL,
   `dimension` VARCHAR(20),
   -- attribut clé étrangères
-  `id_editeur` INT (11) KEY,
+  `id_collection` INT (11) KEY,
   `id_type` INT (11) KEY,
   `id_langue` INT (11) KEY,
   `id_support` INT (11) KEY,
@@ -35,15 +35,13 @@ CREATE TABLE IF NOT EXISTS  `livre` (
 -- -------------------------------------------------------------------------
 
 --
--- Structure de la table editeur
+-- Structure de la table editeur lié aux collections
 --
 
 DROP TABLE IF EXISTS `editeur`;
 CREATE TABLE IF NOT EXISTS `editeur` (
   `id_editeur`INT (11) NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE,
-  `nom` VARCHAR(144) NOT NULL,
-  -- clé étrangère
-  `id_collection` INT (11) KEY
+  `nom` VARCHAR(144) NOT NULL
 )
   ENGINE=InnoDB DEFAULT CHARSET utf8
   COMMENT='Table recenscent tout les editeurs de la base';
@@ -51,13 +49,15 @@ CREATE TABLE IF NOT EXISTS `editeur` (
 -- -------------------------------------------------------------------------
 
 --
--- Structure de la table collection liée seulement à l'éditeur
+-- Structure de la table collection liée à l'éditeur
 --
 
 DROP TABLE IF EXISTS `collection`;
 CREATE TABLE IF NOT EXISTS `collection` (
   `id_collection`INT (11) NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE,
-  `nom` VARCHAR(144) NOT NULL
+  `nom` VARCHAR(144) NOT NULL,
+    -- clé étrangère
+  `id_editeur` INT (11) KEY
 )
   ENGINE=InnoDB DEFAULT CHARSET utf8
   COMMENT='Table recenscent tout les editeurs de la base';
@@ -108,3 +108,65 @@ CREATE TABLE IF NOT EXISTS `support` (
 )
   ENGINE=InnoDB DEFAULT CHARSET utf8
   COMMENT='Table recenscent les support pour chaque livre';
+
+-- -------------------------------------------------------------------------
+
+--
+-- Structure de la table genre
+--
+
+DROP TABLE IF EXISTS `genre`;
+CREATE TABLE IF NOT EXISTS `genre` (
+  `id_genre`INT (11) NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE,
+  `nom` VARCHAR(144) NOT NULL
+)
+  ENGINE=InnoDB DEFAULT CHARSET utf8
+  COMMENT='Table recenscent tout les genres de livre de la base';
+
+-- -------------------------------------------------------------------------
+
+--
+-- Structure de la table origine
+--
+
+DROP TABLE IF EXISTS `origine`;
+CREATE TABLE IF NOT EXISTS `origine` (
+  `id_origine`INT (11) NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE,
+  `nom` VARCHAR(144) NOT NULL
+)
+  ENGINE=InnoDB DEFAULT CHARSET utf8
+  COMMENT='Table inscrivant l"origine du livre dans  la base';
+
+--
+-- la table utilisateur et ses tables exterieures
+--
+-- Structure de la table utilisateur
+--
+
+DROP TABLE IF EXISTS `utilisateur`;
+CREATE TABLE IF NOT EXISTS `utilisateur` (
+  `id_utilisateur` INT (11) NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE,
+  `identifiant` VARCHAR(144) NOT NULL  UNIQUE,
+  `mdp` VARCHAR(144)NOT NULL,
+  `tel` INT (10) NOT NULL,
+  `email` VARCHAR(144) NOT NULL,
+  `nom` VARCHAR(144)NOT NULL,
+  `prenom` VARCHAR(144) NOT NULL ,
+  `dateNaissance` DATE NOT NULL ,
+  `admin` INT(1),
+  -- clé étrangère
+  `id_civilite` INT(11) NOT NULL KEY,
+  `id_adresse` INT(11) NOT NULL KEY
+)
+  ENGINE=InnoDB DEFAULT CHARSET utf8
+  COMMENT='Table contenant chacun des utilsisateur enregistrés';
+
+--
+-- Structure de la table civilité
+--
+
+DROP TABLE IF EXISTS `civilite`;
+CREATE TABLE IF NOT EXISTS `civilite` (
+  `id_civilite` INT(11) NOT NULL PRIMARY KEY UNIQUE,
+  `civilite` VARCHAR(144) NOT NULL 
+)
